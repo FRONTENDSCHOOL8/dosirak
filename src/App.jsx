fc61fd01eff20339dcd2bf928cdf7c17629d0f69
SWLee2973 <kek2973@naver.com>
2024-02-22 14:05:20 +0900

    [Init] 02/22 SWLee2973 프로젝트 초기 생성 #0 - logo 변경 - react-router-dom 6.4 설치 - react-helmet-sync 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import Logo from '/logo.svg';
 
 function App() {
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={Logo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
