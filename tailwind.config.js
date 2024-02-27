1921e592fd1e0d9e335db24e3f2ccbfead1f4afa
반현지 <hyeon@banhyeonjiui-MacBookAir.local>
2024-02-27 13:08:46 +0900

    [Chore] 02/27 lanuioe 테일윈드 스타일 추가 #9 - gray50, gray100 컬러 스타일 추가 - card boxShadow 스타일 추가 - 이미지 어둡게 처리하는 용도의 gradient 스타일 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ export default {
         "secondary-color": "var(--secondary)",
         "tertiary-color": "var(--tertiary)",
         "content-color": "var(--content)",
+        "gray50": "var(--gray-50)",
+        "gray100": "var(--gray-100)",
         "gray300": "var(--gray-300)",
         "gray500": "var(--gray-500)",
         "gray700": "var(--gray-700)",
@@ -32,6 +34,13 @@ export default {
       'paragraph-lg': ['15px', { lineHeight: '150%', letterSpacing: '-1%', fontWeight: '400' }],
       'paragraph-base': ['12px', { lineHeight: '150%', letterSpacing: '-2%', fontWeight: '400' }],
       'paragraph-sm': ['10px', { lineHeight: '150%', letterSpacing: '-2%', fontWeight: '400' }],
+    },
+    boxShadow: {
+      'card': '0px 4px 4px 0px rgba(204, 204, 204, 0.25)',
+    },
+    backgroundImage: {
+      'card-group-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 18.21%, rgba(0, 0, 0, 0.04) 41.52%, rgba(0, 0, 0, 0.53) 84.87%)',
+      'card-feed-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0.00) 49%)',
     },
   },
   plugins: [],
