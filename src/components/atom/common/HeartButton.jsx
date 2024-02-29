e37c326d7dddb341ec760a5eb9c8d30c77dd8c3d
반현지 <hyeon@banhyeonjiui-MacBookAir.local>
2024-02-29 13:15:19 +0900

    [Refactor] 02/29 lanuioe zustand로 상태관리 변경 #9 - 좋아요 버튼 토글 기능 전역 상태관리로 변경

--- src/components/atom/common/HeartButton.jsx
+++ src/components/atom/common/HeartButton.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
+import postStore from '@/store/postStore';
 
-const HeartButton = ({ initialType, className }) => {
-  const [type, setType] = useState(initialType);
+const HeartButton = ({ id, heartColor = 'black', className = '' }) => {
+  // const { heartType, setHeartType } = postStore((state) => state);
+  const { hearts, toggleHeart } = postStore((state) => ({
+    hearts: state.hearts,
+    toggleHeart: state.toggleHeart,
+  }));
 
-  const toggleHeart = () => {
-    setType((currentType) => (currentType === 'true' ? initialType : 'true'));
-  };
+  const heartType = hearts[id] || false;
+
+  let svgName;
+  svgName = heartType ? true : `${false}-${heartColor}`;
+
   return (
-    <button type="button" className={className} onClick={toggleHeart}>
-      <img src={`../src/assets/common/heart-${type}.svg`} alt="좋아요" />
+    <button type="button" className={className} onClick={() => toggleHeart(id)}>
+      <img src={`../src/assets/common/heart-${svgName}.svg`} alt="좋아요" />
     </button>
   );
 };
