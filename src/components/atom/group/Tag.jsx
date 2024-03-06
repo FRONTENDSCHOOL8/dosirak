fedc73609707a6196c41786d664bae0d9845f3b9
lanuioe <lanuioe@gmail.com>
2024-03-06 14:23:27 +0900

    [Refactor] 03/06 lanuioe Tag 컴포넌트 구조 변경 #29 - children을 사용하던 recruit, follow tagType을 props로 변경 - isActive prop으로 boolean값 전달하여 사용

--- src/components/atom/group/Tag.jsx
+++ src/components/atom/group/Tag.jsx
@@ -1,4 +1,4 @@
-const Tag = ({ children, tagType, position = '' }) => {
+const Tag = ({ children, tagType, isActive, position = '' }) => {
   const baseClass = 'rounded-2xl px-3 text-paragraph-base';
 
   const defaultClass = `border border-gray500 py-[6px] text-gray600 ${children === '전체보기' ? 'bg-content-color text-white' : ''}`;
@@ -6,12 +6,21 @@ const Tag = ({ children, tagType, position = '' }) => {
   const typeClasses = {
     delete:
       'flex max-w-fit items-center gap-1 border border-gray500 text-primary-color',
-    recruit: `${children === '모집 중' ? 'bg-content-color' : 'bg-content-transparent'} py-1 text-white`,
-    follow: `${children === '팔로잉' ? 'bg-gray600' : 'bg-primary-color'} py-[6px] text-white`,
+    recruit: `${isActive ? 'bg-content-color' : 'bg-content-transparent'} py-1 text-white`,
+    follow: `${isActive ? 'bg-gray600' : 'bg-primary-color'} py-[6px] text-white`,
+  };
+
+  const typeContent = {
+    recruit: `${isActive ? '모집 중' : '모집 완료'}`,
+    follow: isActive
+      ? { text: '팔로잉', icon: 'check', alt: '체크', label: '해제' }
+      : { text: '팔로우', icon: 'plus', alt: '추가', label: '추가' },
   };
 
   const className =
     `${baseClass} ${typeClasses[tagType] || defaultClass} ${position}`.trim();
+
+  const content = typeContent[tagType];
 
   switch (tagType) {
     case 'delete':
@@ -23,19 +32,20 @@ const Tag = ({ children, tagType, position = '' }) => {
           <button
             type="button"
             aria-label="삭제"
-            className="bg-delete h-4 w-4 bg-cover py-[6px]"
+            className="h-4 w-4 bg-delete bg-cover py-[6px]"
           ></button>
         </li>
       );
 
     case 'recruit':
-      return <span className={className}>{children}</span>;
+      return (
+        <span className={className} isActive={isActive}>
+          {content}
+        </span>
+      );
 
     case 'follow': {
-      const isFollowing = children === '팔로잉';
-      const icon = isFollowing ? 'check' : 'plus';
-      const alt = isFollowing ? '체크' : '추가';
-      const label = isFollowing ? '해제' : '추가';
+      const { text, icon, alt, label } = content;
 
       return (
         <button
@@ -49,7 +59,7 @@ const Tag = ({ children, tagType, position = '' }) => {
               alt={`팔로우 ${alt} 아이콘`}
               className="h-3 w-3"
             />
-            <figcaption>{children}</figcaption>
+            <figcaption>{text}</figcaption>
           </figure>
         </button>
       );
