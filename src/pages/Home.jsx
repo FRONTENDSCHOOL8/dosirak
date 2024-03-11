13b40fec8a7f5b726e32a62a132047d2295e90fa
hyun9758 <jj020305@likelion.org>
2024-03-11 09:18:05 +0900

    [Feat] 03.11 hyun9758 홈 화면 마크업 구현 #47 - 컴퓨터 옮기기 위한 중간 PR

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import MainNavBar from '@/components/molecule/common/MainNavBar';
+import NavBar from '@/components/molecule/navbar/NavBar';
 import { useLoginUserInfo } from '@/hook';
 
 export const Component = () => {
@@ -6,11 +7,22 @@ export const Component = () => {
 
   return (
     <>
-      <section className="relative flex h-fit min-h-screen flex-col">
-        <h2>홈</h2>
-        <span>{userInfo.nickname}님 안녕하세요!</span>
+      <section className="flex h-fit min-h-screen flex-col">
+        <header>
+          <img
+            src="/assets/common/logo.svg"
+            alt=""
+            className="h-[40px] w-[74px]"
+          />
+        </header>
+        <section className="flex ">
+          <section className="relative flex h-fit min-h-screen flex-col">
+            <h2>홈</h2>
+            <span>{userInfo.nickname}님 안녕하세요!</span>
+          </section>
+        </section>
+        <MainNavBar />
       </section>
-      <MainNavBar />
     </>
   );
 };
