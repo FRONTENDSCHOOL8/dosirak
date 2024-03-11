2a4bba090ec9d907bcde1dbe3a6fc6825b5f138b
hyun9758 <jj020305@likelion.org>
2024-03-12 01:08:22 +0900

    [Feat] 03.12 hyun9758 홈 화면 마크업 #47 - 상단 바와 하단 바 구현 - 오프라인 활동 노트북 사용을 위한 푸쉬

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,51 @@
-import MainNavBar from '@/components/molecule/common/MainNavBar';
-import NavBar from '@/components/molecule/navbar/NavBar';
 import { useLoginUserInfo } from '@/hook';
+import { ReactComponent as Bell } from '@/assets/common/alarm.svg';
+import MainNavBar from '@/components/molecule/common/MainNavBar';
+import { Link } from 'react-router-dom';
 
 export const Component = () => {
   const userInfo = useLoginUserInfo();
 
   return (
     <>
-      <section className="flex h-fit min-h-screen flex-col">
-        <header>
-          <img
-            src="/assets/common/logo.svg"
-            alt=""
-            className="h-[40px] w-[74px]"
-          />
-        </header>
-        <section className="flex ">
-          <section className="relative flex h-fit min-h-screen flex-col">
-            <h2>홈</h2>
-            <span>{userInfo.nickname}님 안녕하세요!</span>
-          </section>
+      <header className="fixed top-0 z-10 w-[430px] bg-white shadow-[0_4px_4px_0_rgba(204,204,204,0.25)] mobile:w-screen">
+        <div className="relative flex h-20 items-end justify-between px-9 pb-[18px]">
+          <ul className="">
+            <li>
+              <Link>
+                <img
+                  src="/assets/common/logo.svg"
+                  alt=""
+                  className="h-[40px] w-[74px]"
+                />
+              </Link>
+            </li>
+          </ul>
+          <ul className="flex gap-[22px]">
+            <li className="">
+              <Link to="/">
+                <Bell />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <img src="/assets/common/bookmark-false.svg" alt="북마크" />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+      <main className="">
+        <section className="flex h-[262px] w-[430px] rounded-b-[40px] bg-primary-color">
+          <span className="text-paragraph-lg text-white">
+            {userInfo.nickname}님 안녕하세요!
+          </span>
+          <strong className="text-paragraph-lg text-white">
+            오늘은 이런 도시락 어때요?
+          </strong>
         </section>
-        <MainNavBar />
-      </section>
+      </main>
+      <MainNavBar />
     </>
   );
 };
