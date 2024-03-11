b0b92d59652ea2d076d0cf9fc5d92101ccdbe7df
lanuioe <lanuioe@gmail.com>
2024-03-11 20:20:02 +0900

    [Feat] 03/11 lanuioe NotFound 페이지 마크업 #54 - 페이지 경로가 올바르지 않을 때 제공하는 NotFound 페이지 마크업 - Desktop 환경(1024px 이상)에서 제공하는 QR코드 화면 추가 - '처음으로' 버튼은 SquareButton 컴포넌트 내에서 Link를 사용하여 홈 위치로 이동

--- src/components/atom/common/SquareButton.jsx
+++ src/components/atom/common/SquareButton.jsx
@@ -1,9 +1,16 @@
-const SquareButton = ({ children }) => {
-  return (
-    <button className="noto h-12 w-full rounded-[5px] bg-primary-color text-label text-white">
-      {children}
-    </button>
+import { Link } from 'react-router-dom';
+
+const SquareButton = ({ children, type = 'button' }) => {
+  const baseClass =
+    'noto h-12 w-full rounded-[5px] bg-primary-color text-label text-white';
+  return type === 'link' ? (
+    <Link to="/">
+      <div className={`${baseClass} flex items-center justify-center`}>
+        {children}
+      </div>
+    </Link>
+  ) : (
+    <button className={baseClass}>{children}</button>
   );
 };
-
 export default SquareButton;
