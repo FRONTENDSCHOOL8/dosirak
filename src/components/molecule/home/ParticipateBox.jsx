799b9b558e4fd7daf5aa06647341554cd4fac178
hyun9758 <jj020305@likelion.org>
2024-03-12 21:50:42 +0900

    [Feat] 03.12 hyun9758 피드 개수 데이터 기능 구현 #59 - 피드 개수 데이터 보내는 기능 구현

--- src/components/molecule/home/ParticipateBox.jsx
+++ src/components/molecule/home/ParticipateBox.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
 import ParticipateInfo from './ParticipateInfo';
+import { useEffect } from 'react';
+import { pb } from '@/util';
+import { useLoginUserInfo } from '@/hook';
+
+const INITIAL_COUNT = { group: 0, feed: 0 }; //1-1
+
+const fetchCount = async (userId) => {
+  //4
+  const feedData = await pb.collection('feed').getList(1, 100, {
+    //5
+    filter: `writer.id ?= "${userId}"`, //7
+  });
+
+  return feedData.items.length; //8
+};
 
 const RandomRecipe = () => {
+  const [count, setCount] = useState(INITIAL_COUNT); //1-2
+  const userInfo = useLoginUserInfo(); //6
+  useEffect(() => {
+    //3
+    fetchCount(userInfo.id).then((data) => {
+      //9
+      setCount({ ...count, feed: data }); //10
+    });
+  }, []);
+
   return (
     <section className="mt-[13px] flex">
-      <ParticipateInfo number={2}>모임</ParticipateInfo>
-      <ParticipateInfo number={8}>피드</ParticipateInfo>
+      {/* 2 */}
+      <ParticipateInfo number={count.group}>모임</ParticipateInfo>
+      <ParticipateInfo number={count.feed}>피드</ParticipateInfo>
     </section>
   );
 };
