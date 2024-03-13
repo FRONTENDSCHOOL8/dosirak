608277b11c944d9660eca14fd38df0f92af34238
SWLee2973 <kek2973@naver.com>
2024-03-13 11:30:04 +0900

    [Feat] 03/13 SWLee2973 회원 탈퇴 #52 - 회원 탈퇴 기능 구현 - 탈퇴 flag는 users 컬렉션의 status = 4 - status가 4라면 로그인 시 탈퇴한 회원 표시

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,10 +2,29 @@ import MainNavBar from '@/components/molecule/common/MainNavBar';
 import AlertSetting from '@/components/molecule/mypage/AlertSetting';
 import EtcSetting from '@/components/molecule/mypage/EtcSetting';
 import SettingsHeader from '@/components/organism/mypage/SettingsHeader';
+import { useLoginUserInfo } from '@/hook';
+import { pb } from '@/util';
+import { useNavigate } from 'react-router-dom';
+
+const fetchLeave = async (userId) => {
+  const result = await pb.collection('users').update(userId, { status: 4 });
+
+  return result;
+};
 
 export const Component = () => {
-  const handleLeave = () => {
-    console.log('bye');
+  const userInfo = useLoginUserInfo();
+  const navigate = useNavigate();
+
+  const handleLeave = async () => {
+    if (
+      confirm('정말로 탈퇴하시겠어요?\n피드 글은 자동으로 지워지지 않아요!')
+    ) {
+      await fetchLeave(userInfo.id);
+
+      alert('탈퇴 처리가 완료되었어요.\n다시 만날 날을 기대하고 있을게요.');
+      navigate('/login', { replace: true });
+    }
   };
 
   return (
