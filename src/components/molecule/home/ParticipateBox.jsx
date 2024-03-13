42b1e5b58c3b1b7abeba4aa1f32ee210c41156c3
hyun9758 <jj020305@likelion.org>
2024-03-13 15:47:45 +0900

    [Feat] 03.13 hyun9758 홈 화면 기능 구현 #59 - 홈 화면 내 모임, 내 피드 개수 구현

--- src/components/molecule/home/ParticipateBox.jsx
+++ src/components/molecule/home/ParticipateBox.jsx
@@ -4,36 +4,38 @@ import { useEffect } from 'react';
 import { pb } from '@/util';
 import { useLoginUserInfo } from '@/hook';
 
-const INITIAL_COUNT = { group: 0, feed: 0 }; //1-1
+const INITIAL_COUNT = { groupCount: 0, feedCount: 0 };
 
 const fetchCount = async (userId) => {
-  //4
   const feedData = await pb.collection('feed').getList(1, 100, {
-    //5
-    filter: `writer.id ?= "${userId}"`, //7
+    filter: `writer.id ?= "${userId}"`,
   });
 
-  return feedData.items.length; //8
+  const groupData = await pb.collection('groups').getList(1, 100, {
+    filter: `participant.id ?= "${userId}"`,
+  });
+
+  return {
+    feedCount: feedData.items.length,
+    groupCount: groupData.items.length,
+  };
 };
 
-const RandomRecipe = () => {
-  const [count, setCount] = useState(INITIAL_COUNT); //1-2
-  const userInfo = useLoginUserInfo(); //6
+const ParticipateBox = () => {
+  const [count, setCount] = useState(INITIAL_COUNT);
+  const userInfo = useLoginUserInfo();
   useEffect(() => {
-    //3
     fetchCount(userInfo.id).then((data) => {
-      //9
-      setCount({ ...count, feed: data }); //10
+      setCount(data);
     });
   }, []);
 
   return (
     <section className="mt-[13px] flex">
-      {/* 2 */}
-      <ParticipateInfo number={count.group}>모임</ParticipateInfo>
-      <ParticipateInfo number={count.feed}>피드</ParticipateInfo>
+      <ParticipateInfo number={count.groupCount}>모임</ParticipateInfo>
+      <ParticipateInfo number={count.feedCount}>피드</ParticipateInfo>
     </section>
   );
 };
 
-export default RandomRecipe;
+export default ParticipateBox;
