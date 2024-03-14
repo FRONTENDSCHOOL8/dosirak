f4720fdaec6ce4f21030b5970d644b205c80d08f
SWLee2973 <kek2973@naver.com>
2024-03-14 11:50:13 +0900

    [Modify] 03/14 SWLee2973 내 피드 디자인 수정 #77 - 디자인 시안과 동일하도록 배치

--- src/components/molecule/myfeed/MyPostNavBar.jsx
+++ src/components/molecule/myfeed/MyPostNavBar.jsx
@@ -2,21 +2,15 @@ import { NavLink } from 'react-router-dom';
 
 const MyPostNavBar = () => {
   return (
-    <ul className="mt-[38px] flex w-full text-center">
-      <li className="noto flex-grow border-b-2 border-b-primary-color text-paragraph-lg text-gray500">
-        <NavLink to={'/'} className="">
-          게시글
-        </NavLink>
+    <ul className="noto mt-[38px] flex w-full border-b-[3px] text-center">
+      <li className="relative flex-grow pb-2 text-paragraph-lg after:absolute after:-bottom-1 after:left-0 after:h-[3px] after:w-full after:rounded-lg after:bg-primary-color after:content-['']">
+        <NavLink className="font-bold text-primary-color">게시글</NavLink>
       </li>
-      <li className="noto flex-grow border-b-2 border-b-secondary-color text-paragraph-lg text-gray500">
-        <NavLink to={'/'} className="">
-          임시저장
-        </NavLink>
+      <li className="flex-grow text-paragraph-lg text-gray500">
+        <NavLink className="">임시저장</NavLink>
       </li>
-      <li className="noto flex-grow border-b-2 border-b-tertiary-color text-paragraph-lg text-gray500">
-        <NavLink to={'/'} className="">
-          비공개
-        </NavLink>
+      <li className="flex-grow text-paragraph-lg text-gray500">
+        <NavLink className="">비공개</NavLink>
       </li>
     </ul>
   );
--- src/components/organism/feed/myfeed/MyFeed.jsx
+++ src/components/organism/feed/myfeed/MyFeed.jsx
@@ -5,13 +5,7 @@ import { useLoginUserInfo } from '@/hook';
 import MyFeedProfile from '@/components/molecule/myfeed/MyFeedProfile';
 import MyPostNavBar from '@/components/molecule/myfeed/MyPostNavBar';
 import MyPostGrid from '@/components/molecule/myfeed/MyPostGrid';
-
-const feedPath = [
-  { path: '/feed/popular', children: '인기' },
-  { path: '/feed/recommend', children: '추천' },
-  { path: '/feed/following', children: '팔로잉' },
-  { path: '/feed/myfeed', children: '내 피드' },
-];
+import { feedPath } from '@/pages/Feed';
 
 const MyFeed = ({ feed }) => {
   const userInfo = useLoginUserInfo();
@@ -20,25 +14,25 @@ const MyFeed = ({ feed }) => {
     <>
       <section className="flex h-fit min-h-screen flex-col">
         <header>
-          <NavBar path={feedPath}>피드</NavBar>
+          <NavBar type="feed" path={feedPath}>
+            피드
+          </NavBar>
         </header>
-        <section className="flex h-fit pt-[132px]">
-          <main className="mx-9 justify-center">
-            <MyFeedProfile />
-            <section className=" relative mt-4 flex h-[60px] w-[358px] items-center justify-center rounded-xl bg-primary-color">
-              <p className="text-center text-white">
-                도시락에 내가 좋아하는 것들을 가득 담아..
-              </p>
-              <span className="absolute -bottom-3 right-6 h-[27px] w-[18px]">
-                <Spoon />
-              </span>
-            </section>
-            <div>
-              <MyPostNavBar />
-            </div>
-            <MyPostGrid feed={feed} />
-          </main>
-        </section>
+        <main className="flex h-fit flex-col pt-[132px]">
+          <MyFeedProfile />
+          <section className="noto relative flex w-full items-center justify-center px-12  ">
+            <p className="w-full rounded-lg bg-primary-color p-4 text-center text-white">
+              도시락에 내가 좋아하는 것들을 가득 담아..
+            </p>
+            <span className="absolute -bottom-3 right-16 h-[27px] w-[18px]">
+              <Spoon />
+            </span>
+          </section>
+          <div>
+            <MyPostNavBar />
+          </div>
+          <MyPostGrid feed={feed} />
+        </main>
       </section>
       <MainNavBar />
     </>
